aef9df294107d773e4201e98faf950c98c64227d
Jacson Passold <jacson@ethymos.com.br>
2014-03-20 15:23:51 -0300

    wireframe home + theme debug

---
+++ home_category_list_post.php
@@ -0,0 +1,9 @@
+<?php
+	
+?>
+
+<div class="home-cat-post-box">
+	<span class="home-cat-post-thumbnail" ><?php echo the_post_thumbnail();?></span>
+	<span class="home-cat-post-title" ><?php echo the_title();?></span>
+	<span class="home-cat-post-content" ><?php echo the_excerpt();?></span>
+</div>
