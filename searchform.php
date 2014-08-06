d0059e2ed725732c7e2facc439f16d1142de5b37
Jacson Passold <jacson@ethymos.com.br>
2014-08-06 13:59:11 -0300

    search on home go to solutions catalog

--- searchform.php
+++ searchform.php
@@ -5,6 +5,6 @@
 		<span class="screen-reader-text">Search for:</span>
 		<input type="search" class="search-field" placeholder="<?php echo __('Search').' …'; ?>" value="<?php echo array_key_exists('search', $_REQUEST) ? wp_strip_all_tags($_REQUEST['search']) : '' ; ?>" name="search" title="Search for:" />
 	</label>
-	<input type="hidden" value="<?php echo get_post_type(); ?>" name="post_type" id="post_type" />
+	<input type="hidden" value="<?php echo is_home() ? 'solution' : get_post_type(); ?>" name="post_type" id="post_type" />
 	<input type="submit" class="search-submit" value="Search" style="<?php echo is_home() ? '' : "display: none;"; ?>" />
 </form>
