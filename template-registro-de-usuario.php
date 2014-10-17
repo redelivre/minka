39e533a4b725dc19baf5b0a8eaccfa7a7f5df791
Jacson Passold <jacson@ethymos.com.br>
2014-10-17 15:54:38 -0300

    fix some translations

--- template-registro-de-usuario.php
+++ template-registro-de-usuario.php
@@ -16,26 +16,26 @@
 		<form class="formulario-de-registro-padrao" id="registerform">
 			<div class="campos">
 				<p>
-					<label for="custom-register-username"><?php echo _x('Nome de usuário', 'registro-de-usuario', 'campanha-completa'); ?></label> <br />
+					<label for="custom-register-username"><?php echo _x('Nome de usuário', 'registro-de-usuario', 'minka'); ?></label> <br />
 					<input type="text" id="custom-register-username" class="input" minlength="2" id="user_login" />
 				</p>
 				
 				<p>
-					<label for="custom-register-realname"><?php echo _x('Nome completo', 'registro-de-usuario', 'campanha-completa'); ?></label><br />
+					<label for="custom-register-realname"><?php echo _x('Nome completo', 'registro-de-usuario', 'minka'); ?></label><br />
 					<input type="text" id="custom-register-realname" class="input" />
 				</p>
 				
 				<p>
-					<label for="custom-register-password"><?php echo _x('Senha', 'registro-de-usuario', 'campanha-completa'); ?></label> <br />
+					<label for="custom-register-password"><?php echo _x('Senha', 'registro-de-usuario', 'minka'); ?></label> <br />
 					<input type="password" id="custom-register-password" class="input" />
 				</p>
 				
 				<p>
-					<label for="custom-register-password-review"><?php echo _x('Repita sua senha', 'registro-de-usuario', 'campanha-completa'); ?></label> <br />
+					<label for="custom-register-password-review"><?php echo _x('Repita sua senha', 'registro-de-usuario', 'minka'); ?></label> <br />
 					<input type="password" id="custom-register-password-review" class="input" />
 				</p>
 				<p>
-					<label for="custom-register-email"><?php echo _x('Email', 'registro-de-usuario', 'campanha-completa'); ?></label> <br />
+					<label for="custom-register-email"><?php echo _x('Email', 'registro-de-usuario', 'minka'); ?></label> <br />
 					<input type="email" required="required" id="custom-register-email" name="custom-register-email" class="input" />
 				</p>
 				<p>
@@ -66,7 +66,7 @@
 					
 		</div>
 		<?php else : ?>
-			<?php echo _x('Você está logado neste momento, para efetuar um novo registro será preciso fazer <a href="' . wp_logout_url() . '">logout</a>', 'registro-de-usuario', 'campanha-completa'); ?>
+			<?php echo _x('Você está logado neste momento, para efetuar um novo registro será preciso fazer', 'registro-de-usuario', 'minka');?> <a href="' . wp_logout_url() . '"><?php _x('logout', 'registro-de-usuario', 'minka'); ?></a>'
 		<?php endif; ?>
 	</div>
 <?php do_action( 'after_signup_form' ); ?>
